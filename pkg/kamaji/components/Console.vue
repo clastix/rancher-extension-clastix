e20c781d5a6a623532c2b4fc243d563ecdfdf35b
Ludovico Russo <ludus.russo@gmail.com>
2023-06-11 21:31:59 +0200

    feat: remove iframe

--- pkg/kamaji/components/Console.vue
+++ pkg/kamaji/components/Console.vue
@@ -12,23 +12,10 @@ export default {
 </script>
 
 <template>
-  <iframe
-    class="frame"
-    height="100%"
-    width="100%"
-    :src="computedUrl"
-    sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-forms"
-  ></iframe>
+  <div>
+    <h3>Kamaji Console</h3>
+    <p>Open the <a :href="computedUrl" target="_blank">Kamaji Console</a></p>
+  </div>
 </template>
 
-<style lang="scss" scoped>
-.frame {
-  border: none;
-  margin: 0;
-  position: fixed;
-  top: 55px;
-  bottom: 0;
-  left: 0;
-  right: 0;
-}
-</style>
+<style lang="scss" scoped></style>
